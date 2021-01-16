2835969502bf61129d57ea264449ddbdb52c0899
codeboz <codeboz@outlook.com>
2021-01-16 22:27:17 +0300

    Add ContactEntityTypeConfiguration.cs

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Configuration/ContactEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CBZ.ContactApp.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CBZ.ContactApp.Data.Configuration
+{
+    public class ContactEntityTypeConfiguration:IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {   //Shadow properties
+            builder.Property<DateTime>("Inserted");
+            builder.Property<DateTime>("Updated");
+            builder.Property(c => c.Name).IsRequired();
+            builder.Property(c => c.Surname).IsRequired();
+            //Relations
+            builder.HasMany(c => c.Infos);
+            //Indexes
+            builder.HasIndex(c => new { c.Name, c.Surname });
+        }
+    }
+}
