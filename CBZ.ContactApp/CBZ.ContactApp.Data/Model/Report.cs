fcab84811ad3920d990de985f2a081e59a2f13a4
codeboz <codeboz@outlook.com>
2021-01-16 22:25:42 +0300

    Add Report Data Model

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Model/Report.cs
@@ -0,0 +1,10 @@
+﻿namespace CBZ.ContactApp.Data.Model
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public int ContactCount { get; set; }
+        public int PhoneNumberCount { get; set; }
+        public Info LocationInfo { get; set; }
+    }
+}
