827e989211f6801f329bbfdc8037708e9f9a233b
codeboz <codeboz@outlook.com>
2021-01-16 22:28:13 +0300

    Add ReportRequestEntityTypeConfiguration.cs

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Configuration/ReportRequestEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CBZ.ContactApp.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CBZ.ContactApp.Data.Configuration
+{
+    public class ReportRequestEntityTypeConfiguration:IEntityTypeConfiguration<ReportRequest>
+    {
+        public void Configure(EntityTypeBuilder<ReportRequest> builder)
+        {
+            //Shadow properties
+            builder.Property<DateTime>("Requested");
+            builder.Property<int>("ReportStateId");
+            //Relations
+            builder.HasOne(rr => rr.ReportState);
+        }
+    }
+}
