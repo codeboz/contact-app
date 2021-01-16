7f5fc38905709b18e9c0f11d65d75d69f5f66653
codeboz <codeboz@outlook.com>
2021-01-16 22:25:26 +0300

    Add Info Data Model

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Model/Info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CBZ.ContactApp.Data.Model
+{
+    public class Info
+    {
+        public string Data { get; set; }
+        public int InfoTypeId { get; set; }
+        public virtual InfoType InfoType { get; set; }
+        public Guid ContactId { get; set; }
+        public virtual Contact Contact { get; set; }
+    }
+}
