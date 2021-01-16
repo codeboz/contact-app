b84686780b299b04a694e3397b77c38e384d838a
codeboz <codeboz@outlook.com>
2021-01-16 22:28:49 +0300

    Add ReportRequestEntityTypeConfiguration.cs

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Configuration/ReportStateEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CBZ.ContactApp.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CBZ.ContactApp.Data.Configuration
+{
+    public class ReportStateEntityTypeConfiguration:IEntityTypeConfiguration<ReportState>
+    {
+        public void Configure(EntityTypeBuilder<ReportState> builder)
+        {
+            //Shadow properties
+            builder.Property<DateTime>("Inserted");
+            builder.Property<DateTime>("Updated");
+
+            //Relations
+            builder.HasMany(rs => rs.ReportRequest);
+            //Indexes
+            builder.HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
