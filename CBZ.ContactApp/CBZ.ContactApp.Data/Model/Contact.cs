80cdb5d2d3494963b58efb8a9bda8eae8cd96e01
codeboz <codeboz@outlook.com>
2021-01-16 22:25:12 +0300

    Add contact Data Model

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Model/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CBZ.ContactApp.Data.Model
+{
+    public class Contact
+    {
+        public  Guid Id { get; set; }
+        public  string Name { get; set; }
+        public  string Surname { get; set; }
+        public  string Company { get; set; }
+        public virtual ICollection<Info> Infos { get; set; }
+    }
+}
