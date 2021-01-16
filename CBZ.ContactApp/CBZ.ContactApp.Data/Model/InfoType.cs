6534ea97b51ef56009fbf743b55a83134a65bf11
codeboz <codeboz@outlook.com>
2021-01-16 22:25:33 +0300

    Add InfoType Data Model

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Model/InfoType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CBZ.ContactApp.Data.Model
+{
+    public class InfoType
+    {
+        public  int Id { get; set; }
+        public  string Name { get; set; }
+        public ICollection<Info> Infos { get; set; }
+    }
+}
