b07328f2163924fb75ecb9c6cb176691faa556ee
codeboz <codeboz@outlook.com>
2021-01-16 22:27:42 +0300

    Add InfoEntityTypeConfiguration.cs

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Configuration/InfoTypeEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CBZ.ContactApp.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CBZ.ContactApp.Data.Configuration
+{
+    public class InfoTypeEntityTypeConfiguration:IEntityTypeConfiguration<InfoType>
+    {
+        public void Configure(EntityTypeBuilder<InfoType> builder)
+        {
+            //Shadow properties
+            builder.Property<DateTime>("Inserted");
+            builder.Property<DateTime>("Updated");
+            builder.Property(i => i.Name).IsRequired();
+            //Relations
+            builder.HasMany(it => it.Infos);
+            //Indexes
+            builder.HasIndex(it => it.Name).IsUnique();
+
+        }
+    }
+}
