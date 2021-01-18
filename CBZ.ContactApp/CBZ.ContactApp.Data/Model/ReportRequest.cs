b32c3f7ce375f825add2a316186804e0f0362567
codeboz <codeboz@outlook.com>
2021-01-18 22:42:18 +0300

    Make Fk nullable

--- CBZ.ContactApp/CBZ.ContactApp.Data/Model/ReportRequest.cs
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Model/ReportRequest.cs
@@ -4,13 +4,13 @@ namespace CBZ.ContactApp.Data.Model
 {
     public class ReportRequest
     {
-        public  Guid Id { get; set; }
-        public  string Location { get; set; }
+        public Guid Id { get; set; }
+        public string Location { get; set; }
         public DateTime Requested { get; set; }
-        public int ReportStateId { get; set; }
+        public int? ReportStateId { get; set; }
         public virtual ReportState ReportState { get; set; }
         
-        public  int ReportId { get; set; }
+        public  int? ReportId { get; set; }
         
         public virtual Report Report { get; set; }
     }
