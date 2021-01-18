b55020c3a2c183735a165b1921ec379af0eb0977
codeboz <codeboz@outlook.com>
2021-01-18 22:44:04 +0300

    Update migration.

---
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Migrations/20210118193541_ReportRequestsDefaultValues.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CBZ.ContactApp.Data.Migrations
+{
+    public partial class ReportRequestsDefaultValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReportRequests_Reports_ReportId",
+                table: "ReportRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReportRequests_ReportStates_ReportStateId",
+                table: "ReportRequests");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "ReportStates",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "ReportStates",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "Reports",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "Reports",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Requested",
+                table: "ReportRequests",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReportStateId",
+                table: "ReportRequests",
+                type: "integer",
+                nullable: true,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReportId",
+                table: "ReportRequests",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "InfoTypes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "InfoTypes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "Contacts",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "Contacts",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReportRequests_Reports_ReportId",
+                table: "ReportRequests",
+                column: "ReportId",
+                principalTable: "Reports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReportRequests_ReportStates_ReportStateId",
+                table: "ReportRequests",
+                column: "ReportStateId",
+                principalTable: "ReportStates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReportRequests_Reports_ReportId",
+                table: "ReportRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReportRequests_ReportStates_ReportStateId",
+                table: "ReportRequests");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "ReportStates",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "ReportStates",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "Reports",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "Reports",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Requested",
+                table: "ReportRequests",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReportStateId",
+                table: "ReportRequests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true,
+                oldDefaultValue: 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReportId",
+                table: "ReportRequests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "InfoTypes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "InfoTypes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "Contacts",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "Contacts",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "NOW()");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReportRequests_Reports_ReportId",
+                table: "ReportRequests",
+                column: "ReportId",
+                principalTable: "Reports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReportRequests_ReportStates_ReportStateId",
+                table: "ReportRequests",
+                column: "ReportStateId",
+                principalTable: "ReportStates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
