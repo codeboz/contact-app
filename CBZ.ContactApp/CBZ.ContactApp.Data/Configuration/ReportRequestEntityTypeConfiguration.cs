130e2e74b4299dace1ebbc9fdd4a8ba4883015e7
codeboz <codeboz@outlook.com>
2021-01-18 05:08:20 +0300

    Assign Constant Guid To ReportRequest Seed for test purposes.

--- CBZ.ContactApp/CBZ.ContactApp.Data/Configuration/ReportRequestEntityTypeConfiguration.cs
+++ CBZ.ContactApp/CBZ.ContactApp.Data/Configuration/ReportRequestEntityTypeConfiguration.cs
@@ -12,8 +12,8 @@ namespace CBZ.ContactApp.Data.Configuration
         public static IEnumerable<ReportRequest> ReportRequestSeed =>
             new List<ReportRequest>
             {
-                new ReportRequest{Id = Guid.NewGuid(), Location = "Ankara",ReportStateId = 1},
-                new ReportRequest{Id = Guid.NewGuid(), Location = "Bursa",ReportStateId = 2},
+                new ReportRequest{Id = Guid.Parse("bf5bd4b9-ebf5-41a8-a597-69b7aeb41432"), Location = "Ankara",ReportStateId = 1},
+                new ReportRequest{Id = Guid.Parse("4c802ae2-5292-4063-b5ca-e09b18871143"), Location = "Bursa",ReportStateId = 2},
             };
         public void Configure(EntityTypeBuilder<ReportRequest> builder)
         {
